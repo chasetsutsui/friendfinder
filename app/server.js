51445cd922703a222108b4fe0d6d714d9b3e7c2e
Chase Tsutsui <chasetsutsui@yahoo.com>
2019-12-27 18:37:47 -0800

    progress on html pages

--- app/server.js
+++ app/server.js
@@ -4,3 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+const jsonParser = bodyParser.json();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.listen(PORT, function(){
+    console.log("app listening on PORT: " + PORT);
+})
